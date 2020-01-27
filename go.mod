0a09900550c7cbef9e74a2b505e1ea8643fdece2
tsak <1022521+tsak@users.noreply.github.com>
2020-01-27 20:27:12 +0000

    First version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tsak/allcaps
+
+go 1.13
+
+require github.com/nlopes/slack v0.6.1-0.20191106133607-d06c2a2b3249
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nlopes/slack"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var evilLowerCase = regexp.MustCompile("[a-z]")
+
+func getenv(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		panic("missing required environment variable " + name)
+	}
+	return v
+}
+
+func main() {
+	token := getenv("SLACKTOKEN")
+	api := slack.New(token)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			//fmt.Printf("Event Received: %s %+v\n", msg.Type, msg.Data)
+			switch ev := msg.Data.(type) {
+
+			case *slack.MessageEvent:
+				info := rtm.GetInfo()
+
+				matched := evilLowerCase.MatchString(ev.Text)
+
+				if ev.User != info.User.ID && matched {
+					rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("*ALL CAPS PLEASE!* %s", strings.ToUpper(ev.Text)), ev.Channel))
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+				// Take no action
+			}
+		}
+	}
+}
