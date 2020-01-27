3f92f80c01c64167b26fba48c43e902ec6b613a6
tsak <1022521+tsak@users.noreply.github.com>
2020-01-27 23:08:27 +0000

    REFACTOR DETECTION ALGORITHM TO BE A BIT MORE STATELESS
    ALSO TDD!

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"github.com/nlopes/slack"
 	"os"
-	"regexp"
 	"strings"
 )
 
-var evilLowerCase = regexp.MustCompile("[a-z]")
+var debug = false
 
 func getenv(name string) string {
 	v := os.Getenv(name)
@@ -16,6 +15,99 @@ func getenv(name string) string {
 		panic("missing required environment variable " + name)
 	}
 	return v
+}
+
+// Debug enables or disables debug mode
+func Debug(state bool) {
+	debug = state
+}
+
+// ContainsLowercase signals if a given string contains lowercase, ignoring Slack @mentions, #channels, URLs and :emoji:
+func ContainsLowercase(m string) bool {
+	// Split string into chars
+	chars := strings.Split(m, "")
+	l := len(chars)
+	out := ""
+	before := ""
+
+	// State
+	channel := false
+	mention := false
+	url := false
+	emoji := false
+
+	for i, char := range chars {
+		switch char {
+		// #channel
+		case "#":
+			if url {
+				break
+			}
+			if before == "" || before == " " {
+				channel = true
+			} else {
+				out += "#"
+			}
+		// @mention
+		case "@":
+			if url {
+				break
+			}
+			if before == "" || before == " " {
+				mention = true
+			} else {
+				out += "@"
+			}
+		// URL
+		case "h":
+			if channel || mention || url || emoji {
+				break
+			}
+			if (before == "" || before == " ") && (l-i > 7 && strings.Join(chars[i:i+7], "") == "http://" || l-i > 8 && strings.Join(chars[i:i+8], "") == "https://") {
+				url = true
+			} else {
+				out += "h"
+			}
+		// :emoji:
+		case ":":
+			if url {
+				break
+			}
+			if emoji {
+				emoji = false
+				break
+			}
+			if before == "" || before == " " {
+				emoji = true
+			} else {
+				out += ":"
+			}
+		// Terminate when seeing a space
+		case " ":
+			if channel || mention || url || emoji {
+				channel = false
+				mention = false
+				url = false
+				emoji = false
+			}
+			out += " "
+		default:
+			if !(channel || mention || url || emoji) {
+				out += char
+			}
+		}
+
+		if debug {
+			fmt.Printf("'%s' '%s' '%s' (%t %t %t %t) %d %d %d\n", char, before, out, channel, mention, url, emoji, i, l, l-i)
+		}
+
+		before = char
+	}
+
+	if debug {
+		fmt.Printf("'%s' => '%s'\n", m, out)
+	}
+	return strings.ToUpper(out) != out
 }
 
 func main() {
@@ -34,9 +126,7 @@ Loop:
 			case *slack.MessageEvent:
 				info := rtm.GetInfo()
 
-				matched := evilLowerCase.MatchString(ev.Text)
-
-				if ev.Msg.User != info.User.ID && ev.SubType == "" && matched {
+				if ev.Msg.User != info.User.ID && ev.SubType == "" && ContainsLowercase(ev.Text) {
 					rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("*ALL CAPS PLEASE!* %s", strings.ToUpper(ev.Text)), ev.Channel))
 				}
 
---
+++ main_test.go
@@ -0,0 +1,86 @@
+package main
+
+import "testing"
+
+func TestContainsLowercase(t *testing.T) {
+	//Debug(true)
+	tests := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{name: "nil", input: "", expected: false},
+		{name: "simple lowercase", input: "foo", expected: true},
+		{name: "SIMPLE UPPERCASE", input: "BAR", expected: false},
+		// #channels
+		{name: "simple #channel", input: "#channel", expected: false},
+		{name: "#channel at beginning with lowercase", input: "#channel bar", expected: true},
+		{name: "#channel at beginning with UPPERCASE", input: "#channel BAR", expected: false},
+		{name: "#channel in middle, surrounded by lowercase", input: "foo #channel bar", expected: true},
+		{name: "#channel in middle, surrounded by UPPERCASE", input: "FOO #channel BAR", expected: false},
+		{name: "#channel at end", input: "FOO #channel", expected: false},
+		{name: "#channel no space", input: "foo#channel", expected: true},
+		{name: "#channel no space", input: "FOO#channel", expected: true},
+		{name: "#channel no space", input: "FOO#CHANNEL", expected: false},
+		// @mention
+		{name: "simple @mention", input: "@mention", expected: false},
+		{name: "@mention at beginning with lowercase", input: "@mention bar", expected: true},
+		{name: "@mention at beginning with UPPERCASE", input: "@mention BAR", expected: false},
+		{name: "@mention in middle, surrounded by lowercase", input: "foo @mention bar", expected: true},
+		{name: "@mention in middle, surrounded by UPPERCASE", input: "FOO @mention BAR", expected: false},
+		{name: "@mention at end", input: "FOO @mention", expected: false},
+		{name: "@mention no space", input: "foo@mention", expected: true},
+		{name: "@mention no space", input: "FOO@mention", expected: true},
+		{name: "@mention no space", input: "FOO@MENTION", expected: false},
+		// HTTP URL
+		{name: "simple http://user:pass@domain.com#anchor", input: "http://user:pass@domain.com#anchor", expected: false},
+		{name: "http://user:pass@domain.com#anchor at beginning with lowercase", input: "http://user:pass@domain.com#anchor bar", expected: true},
+		{name: "http://user:pass@domain.com#anchor at beginning with UPPERCASE", input: "http://user:pass@domain.com#anchor BAR", expected: false},
+		{name: "http://user:pass@domain.com#anchor in middle, surrounded by lowercase", input: "foo http://user:pass@domain.com#anchor bar", expected: true},
+		{name: "http://user:pass@domain.com#anchor in middle, surrounded by UPPERCASE", input: "FOO http://user:pass@domain.com#anchor BAR", expected: false},
+		{name: "http://user:pass@domain.com#anchor at end", input: "FOO http://user:pass@domain.com#anchor", expected: false},
+		{name: "http://user:pass@domain.com#anchor no space", input: "foohttp://user:pass@domain.com#anchor", expected: true},
+		{name: "http://user:pass@domain.com#anchor no space", input: "FOOhttp://user:pass@domain.com#anchor", expected: true},
+		// HTTPS URL
+		{name: "simple https://user:pass@domain.com#anchor", input: "https://user:pass@domain.com#anchor", expected: false},
+		{name: "https://user:pass@domain.com#anchor at beginning with lowercase", input: "https://user:pass@domain.com#anchor bar", expected: true},
+		{name: "https://user:pass@domain.com#anchor at beginning with UPPERCASE", input: "https://user:pass@domain.com#anchor BAR", expected: false},
+		{name: "https://user:pass@domain.com#anchor in middle, surrounded by lowercase", input: "foo https://user:pass@domain.com#anchor bar", expected: true},
+		{name: "https://user:pass@domain.com#anchor in middle, surrounded by UPPERCASE", input: "FOO https://user:pass@domain.com#anchor BAR", expected: false},
+		{name: "https://user:pass@domain.com#anchor at end", input: "FOO https://user:pass@domain.com#anchor", expected: false},
+		{name: "https://user:pass@domain.com#anchor no space", input: "foohttps://user:pass@domain.com#anchor", expected: true},
+		{name: "https://user:pass@domain.com#anchor no space", input: "FOOhttps://user:pass@domain.com#anchor", expected: true},
+		// :emoji:
+		{name: "simple :emoji:", input: ":emoji:", expected: false},
+		{name: ":emoji: at beginning with lowercase", input: ":emoji: bar", expected: true},
+		{name: ":emoji: at beginning with UPPERCASE", input: ":emoji: BAR", expected: false},
+		{name: ":emoji: in middle, surrounded by lowercase", input: "foo :emoji: bar", expected: true},
+		{name: ":emoji: in middle, surrounded by UPPERCASE", input: "FOO :emoji: BAR", expected: false},
+		{name: ":emoji: at end", input: "FOO :emoji:", expected: false},
+		{name: ":emoji: no space", input: "foo:emoji:", expected: true},
+		{name: ":emoji: no space", input: "FOO:emoji:", expected: true},
+		{name: ":emoji: no space", input: "FOO:EMOJI:", expected: false},
+		// Mix
+		{name: "#channelmoji:", input: "#channelmoji:", expected: true},
+		{name: ":emoji#chan", input: ":emoji#chan", expected: true},
+		{name: ":emoji@mention", input: ":emoji@mention", expected: true},
+		{name: "@mention:moji", input: "@mention:moji", expected: true},
+		{name: "@mention:moji:", input: "@mention:moji:", expected: true},
+		// Unicode
+		{name: "sᴍᴀʟʟ ᴄᴀᴘs", input: "sᴍᴀʟʟ ᴄᴀᴘs", expected: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContainsLowercase(tt.input)
+
+			if got != tt.expected {
+				if tt.expected {
+					t.Errorf("'%s' was not detected as lowercase", tt.input)
+				} else {
+					t.Errorf("'%s' WAS NOT DETECTED AS UPPERCASE", tt.input)
+				}
+			}
+		})
+	}
+}
